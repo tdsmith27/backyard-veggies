525183f2ea612c6f2f10a24020232a76e9d84e0b
Trevor Smith <smittrevor@gmail.com>
2019-04-30 21:35:10 -0700

    update webpack config to minimize bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 module.exports = {
   entry: __dirname + "/client/index.jsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/public"
+  },
   module: {
     rules: [
       {
@@ -8,11 +16,50 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader",
+          options: {
+            attrs: [":data-src"],
+            minimize: true
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg|ttf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/public"
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "James's House of Bundles",
+      template: __dirname + "/client/index.html", //create index.html with js script
+      inject: "body",
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      //minify that css
+      filename: "[name]-[hash].css",
+      chunkFilename: "[id][hash].css"
+    }),
+    new UglifyJsPlugin({ sourceMap: true }) //smash everything
+  ],
+  mode: "development"
 };
