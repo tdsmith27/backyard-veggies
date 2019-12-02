6e009648dfa476b8b178b4ff9cf28de1411acf1f
Trevor Smith <smittrevor@gmail.com>
2019-12-02 15:52:34 -0800

    add sample.env file, update db/index to obfuscate DB_STRING

--- db/index.js
+++ db/index.js
@@ -3,9 +3,7 @@ const util = require("util");
 require("dotenv").config();
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USR}:${
-    process.env.DB_PASS
-  }@seasonal-recipe-finder-zg3vg.mongodb.net/test?retryWrites=true`,
+  `mongodb+srv://${process.env.DB_USR}:${process.env.DB_PASS}@${process.env.DB_STRING}?retryWrites=true`,
   () => console.log("database connection established")
 );
 
